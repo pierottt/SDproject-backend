8ac1218f970157cc45a185d1ca3c783c40afe5d6
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-03-02 13:30:38 +0700

    rearranging code

--- routes/rents.js
+++ routes/rents.js
@@ -9,6 +9,7 @@ const {protect,authorize}  =require('../middleware/auth');
 
 router.route('/').get(protect,getRents).post(protect,authorize('admin','user'),addRent);
 
-router.route('/:id').get(protect,getRent).put(protect,authorize('admin','user'),updateRent).delete(protect,authorize('admin','user'),deleteRent);
+router.route('/:id').get(protect,getRent).put(protect,authorize('admin','user'),updateRent)
+                    .delete(protect,authorize('admin','user'),deleteRent);
 
 module.exports=router;
